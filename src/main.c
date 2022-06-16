268369d3980bdc309ac2a5ee3247076890b79928
R3tr074 <jobuzetiuol123@gmail.com>
2022-06-16 17:20:59 -0300

    Add multiple parms

--- src/main.c
+++ src/main.c
@@ -11,24 +11,29 @@ int main(int argc, char *argv[]) {
     exit(0);
   }
 
-  elf_ctx_t context;
-  int ret;
+  int prog_ret = EXIT_SUCCESS;
+  for (int i = 1; i < argc; i++) {
+    elf_ctx_t context;
+    int ret;
 
-  ret = elf_load_file(&context, argv[1]);
-  if (ret != 0) {
-    fprintf(stderr, "Error to open \"%s\" file\n", argv[1]);
-    return EXIT_FAILURE;
+    ret = elf_load_file(&context, argv[i]);
+    if (ret != 0) {
+      fprintf(stderr, "Error to open \"%s\" file\n", argv[1]);
+      prog_ret = EXIT_FAILURE;
+      continue;
+    }
+
+    ret = elf_parse(&context);
+    if (ret != 0) {
+      fprintf(stderr, "Error to parse \"%s\" file\nthis is really a elf?\n",
+              argv[1]);
+      prog_ret = EXIT_FAILURE;
+      continue;
+    }
+
+    format_context(&context);
+
+    elf_unload_file(&context);
   }
-
-  ret = elf_parse(&context);
-  if (ret != 0) {
-    fprintf(stderr, "Error to parse \"%s\" file\nthis is really a elf?\n",
-            argv[1]);
-    return EXIT_FAILURE;
-  }
-
-  format_context(&context);
-
-  elf_unload_file(&context);
-  return EXIT_SUCCESS;
+  return prog_ret;
 }
